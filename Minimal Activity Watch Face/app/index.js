49dd55f326ad681ec1a648ca3c08ddaf0e72c685
Hanna Rakhsha <hanna.rakhsha@gmail.com>
2019-09-05 18:22:06 -0500

    Updated specific function calls to enhance battery life

--- Minimal Activity Watch Face/app/index.js
+++ Minimal Activity Watch Face/app/index.js
@@ -2,16 +2,18 @@
 import document from "document";
 import {preferences} from "user-settings";
 import * as util from "../common/utils";
+//Display import
+import {display} from "display";
 //Clock import
 import clock from "clock";
 //Activity imports
 import {me as appbit} from "appbit";
 import {today} from "user-activity";
 //Battery import
-import { battery } from "power";
+import {battery} from "power";
 //Heart rate imports
-import { HeartRateSensor } from "heart-rate";
-import { BodyPresenceSensor } from "body-presence";
+import {HeartRateSensor} from "heart-rate";
+import {BodyPresenceSensor} from "body-presence";
 
 
 // Get a handle on the <text> elements
@@ -29,6 +31,8 @@ const elevation = document.getElementById("elevation");
 const calories = document.getElementById("calories");
 const activeMin = document.getElementById("activeMin");
 const batteryLevel = document.getElementById("batteryLevel");
+let swapRight = document.getElementById("swapRight");
+let swapLeft = document.getElementById("swapLeft");
 
 //Get a handle on the <image> elements from the index.gui file
 var Heart = document.getElementById("Heart");
@@ -39,125 +43,87 @@ var Distance = document.getElementById("Distance");
 var Elevation = document.getElementById("Elevation");
 var ActiveMinutes = document.getElementById("ActiveMinutes");
 
+
 // Update the clock every second
 clock.granularity = "seconds";
 
 // Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-    hours = util.zeroPad(hours);
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
+function updateClock(){
+  clock.ontick = (evt) => {
+    let today = evt.date;
+    let hours = today.getHours();
+    if (preferences.clockDisplay === "12h"){
+      // 12h format
+      hours = hours % 12 || 12;
+      hours = util.zeroPad(hours);
+    }
+    else{
+      // 24h format
+      hours = util.zeroPad(hours);
+    }
+    let mins = util.zeroPad(today.getMinutes());
+    let sec = util.zeroPad(today.getSeconds());
+
+    clockHour.text = `${hours}:`;
+    clockMin.text = `${mins}`;
+    clockSec.text = `${sec}`;
+
+    date.text = `${days[today.getDay()]}, ${months[today.getMonth()]} ${today.getDate()}`;
   }
-  let mins = util.zeroPad(today.getMinutes());
-  let sec = util.zeroPad(today.getSeconds());
-  
-  clockHour.text = `${hours}:`;
-  clockMin.text = `${mins}`;
-  clockSec.text = `${sec}`;
-  
-  //Function call to keep steps updated at all times
-  updateSteps();
 }
-
-//Update date daily
-function updateDate() {
-  let today = new Date();
-  date.text = `${days[today.getDay()]}, ${months[today.getMonth()]} ${today.getDate()}`;
-}
-
-//Function call
-updateDate();
-
 
 //Update heart rate depending on if watch is on or off body
-if (BodyPresenceSensor) {
+if (BodyPresenceSensor){
   const body = new BodyPresenceSensor();
   const hrm = new HeartRateSensor({ frequency: 1 });
   body.addEventListener("reading", () => {
     //If the user is not wearing the watch display no heart rate
-    if (!body.present) {
+    if (!body.present){
       hrm.stop();
       heartRate.text = "--";
-    //If the user is wearing the watch display heart rate
-    } else {
-      if (HeartRateSensor) {
+      //If the user is wearing the watch display heart rate
+    }
+    else{
+      if (HeartRateSensor){
         hrm.addEventListener("reading", () => {
-        heartRate.text = hrm.heartRate;
-        });
+          heartRate.text = hrm.heartRate;
+          });
         hrm.start();
       }
     }
-  });
+    });
   body.start();
 }
 
-//Image display for heart rate
-Heart.style.display = "inline";
 
-
-//Initialize battery level and image to show
+//Initialize battery level and create function to update battery
+//as the battery level changes
 batteryLevel.text = `${battery.chargeLevel}%`;
-Battery.style.display = "inline";
 
-//Change battery level as battery charge level changes
-battery.onchange = (charger, evt) => {
-  batteryLevel.text = `${battery.chargeLevel}%`;
+function updateBattery(){
+  battery.onchange = (charger, evt) => {
+    batteryLevel.text = `${battery.chargeLevel}%`;
+  }
 }
 
 
-//If user grants permission each below activity information will be retrieved
-function updateSteps(){
-  if (appbit.permissions.granted("access_activity")) {
+//If user grants permission activity information will be retrieved
+function updateActivities(){
+  if (appbit.permissions.granted("access_activity")){
     steps.text = today.adjusted.steps;
-  }
-  else {
-  steps.text = "No data";
-  }
-}
-
-function updateDistance(){
-  if (appbit.permissions.granted("access_activity")) {
     distance.text = today.adjusted.distance;
-  }
-  else {
-  distance.text = "No data";
-  }
-}
-
-function updateElevation(){
-  if (appbit.permissions.granted("access_activity")) {
     elevation.text = today.adjusted.elevationGain;
-  }
-  else {
-  elevation.text = "No data";
-  }
-}
-
-function updateCalories(){
-  if (appbit.permissions.granted("access_activity")) {
     calories.text = today.adjusted.calories;
-  }
-  else {
-  calories.text = "No data";
-  }
-}
-
-function updateActiveMinutes(){
-  if (appbit.permissions.granted("access_activity")) {
     activeMin.text = today.adjusted.activeMinutes;
   }
-  else {
-  activeMin.text = "No data";
+  else{
+    steps.text = "No data";
+    distance.text = "No data";
+    elevation.text = "No data";
+    calories.text = "No data";
+    activeMin.text = "No data";
   }
 }
-
-
 
 //Initializes visibility of images and text
 steps.style.visibility = "visible";
@@ -167,43 +133,90 @@ elevation.style.visibility = "hidden";
 calories.style.visibility = "hidden";
 activeMin.style.visibility = "hidden";
 
-//Visibility of text and images for activities,
-//dependent on screen click
-let swap = document.getElementById("swap");
-
-swap.onclick = function(e) {
+//Visibility of text and images for activities, dependent on screen click
+//Click through back and forth between activities
+swapRight.onclick = function(e){
   if(steps.style.visibility === "visible"){
     steps.style.visibility = "hidden";
-    distance.style.visibility = "visible";
     Steps.style.display = "none";
+    distance.style.visibility = "visible";
     Distance.style.display = "inline";
-    updateDistance();
   }
   else if(distance.style.visibility === "visible"){
     distance.style.visibility = "hidden";
-    elevation.style.visibility = "visible";
     Distance.style.display = "none";
+    elevation.style.visibility = "visible";
     Elevation.style.display = "inline";
-    updateElevation();
   }
   else if(elevation.style.visibility === "visible"){
     elevation.style.visibility = "hidden";
-    calories.style.visibility = "visible";
     Elevation.style.display = "none";
+    calories.style.visibility = "visible";
     Calories.style.display = "inline";
-    updateCalories();
   }
   else if(calories.style.visibility === "visible"){
     calories.style.visibility = "hidden";
-    activeMin.style.visibility = "visible";
     Calories.style.display = "none";
+    activeMin.style.visibility = "visible";
     ActiveMinutes.style.display = "inline";
-    updateActiveMinutes();
   }
   else{
-    steps.style.visibility = "visible";
     activeMin.style.visibility = "hidden";
     ActiveMinutes.style.display = "none";
+    steps.style.visibility = "visible";
     Steps.style.display = "inline";
+  }
+}
+
+swapLeft.onclick = function(e){
+  if(steps.style.visibility === "visible"){
+    steps.style.visibility = "hidden";
+    Steps.style.display = "none";
+    activeMin.style.visibility = "visible";
+    ActiveMinutes.style.display = "inline";
+  }
+  else if(activeMin.style.visibility === "visible"){
+    activeMin.style.visibility = "hidden";
+    ActiveMinutes.style.display = "none";
+    calories.style.visibility = "visible";
+    Calories.style.display = "inline";
+  }
+  else if(calories.style.visibility === "visible"){
+    calories.style.visibility = "hidden";
+    Calories.style.display = "none";
+    elevation.style.visibility = "visible";
+    Elevation.style.display = "inline";
+  }
+  else if(elevation.style.visibility === "visible"){
+    elevation.style.visibility = "hidden";
+    Elevation.style.display = "none";
+    distance.style.visibility = "visible";
+    Distance.style.display = "inline";
+  }
+  else{
+    distance.style.visibility = "hidden";
+    Distance.style.display = "none";
+    steps.style.visibility = "visible";
+    Steps.style.display = "inline";
+  }
+}
+
+
+//Update and display activity only if display is on or switched on
+if(display.on){
+  updateClock();
+  updateActivities();
+  updateBattery();
+  display.onchange = (evt) => {
+    updateClock();
+    updateActivities();
+    updateBattery();
+  }
+}
+else{
+  display.onchange = (evt) => {
+  updateClock();
+  updateActivities();
+  updateBattery();
   }
 }
